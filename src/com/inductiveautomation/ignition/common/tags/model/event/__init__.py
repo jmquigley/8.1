a3e875e8823c99d56148d67c2aab4d8b0e6a7da6
James Quigley <jquigley@deltarfasteners.com>
2023-01-30 16:36:33 -0600

    Added missiong stubs for a tag listener

---
+++ src/com/inductiveautomation/ignition/common/tags/model/event/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["TagChangeListener"]
+
+
+class TagChangeListener(object):
+    pass
---
+++ src/com/inductiveautomation/ignition/gateway.py
@@ -0,0 +1,6 @@
+#
+# Stub code to please Ignition system code in local dev
+#
+
+class IgnitionGateway(object):
+    pass
